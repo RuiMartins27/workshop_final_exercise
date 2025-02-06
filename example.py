6841446f9beae85251522c4ee3b9b044d1b9c14b
RuiMartins27 <rui.piresmartins@maastrichtuniversity.nl>
2025-02-06 12:09:08 +0100

    added the python file

---
+++ example.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+def test_add():
+    assert add(1,1) == 2
+    assert add(1,-1) == 0
+    assert add(5.5,4.5) == 10
