b63597923b46d2863aa2ae2120ff7be87ec549ea
RuiMartins27 <rui.piresmartins@maastrichtuniversity.nl>
2025-02-06 12:28:19 +0100

    separated tests

--- test_example.py
+++ test_example.py
@@ -1,10 +1,11 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a + b  # do not change this line until prompted to do so.
+from example import add, subtract
 
 def test_add():
     assert add(1,1) == 2
     assert add(1,-1) == 0
     assert add(5.5,4.5) == 10
+
+def test_subtract():
+    assert add(1,1) == 0
+    assert add(1,-1) == 2
+    assert add(5.5,4.5) == 1.01
